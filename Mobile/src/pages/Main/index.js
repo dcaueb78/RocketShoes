fa5c555c9555f9c06610a591f525b8354bab8295
dcaueb78 <caue1017@hotmail.com>
2019-12-01 19:23:53 -0200

    Created products list

--- Mobile/src/pages/Main/index.js
+++ Mobile/src/pages/Main/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable react/state-in-constructor */
 import React, {Component} from 'react';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {formatPrice} from '../../util/format';
+// Não utilizando API por utilizar aparelho físico na emulação
+// import api from '../../services/api';
 
 import {
   ProductList,
@@ -18,32 +20,70 @@ import {
 
 export default class Main extends Component {
   state = {
-    items: [
-      {name: 'Tênis muito massaaaa'},
-      {name: 'Tênis muito massaaaa2'},
-      {name: 'Tênis muito massaaaa3'},
+    products: [
+      {
+        id: 1,
+        title: 'Tênis de Caminhada Leve Confortável',
+        price: 179.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+      },
+      {
+        id: 2,
+        title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+        price: 139.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+      },
+      {
+        id: 3,
+        title: 'Tênis Adidas Duramo Lite 2.0',
+        price: 219.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
+      },
+      {
+        id: 5,
+        title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+        price: 139.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+      },
+      {
+        id: 6,
+        title: 'Tênis Adidas Duramo Lite 2.0',
+        price: 219.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
+      },
+      {
+        id: 4,
+        title: 'Tênis de Caminhada Leve Confortável',
+        price: 179.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+      },
     ],
   };
 
   render() {
-    const {items} = this.state;
+    const {products} = this.state;
 
     return (
       <ProductList>
         <List
           horizontal
-          data={items}
-          keyExtractor={item => item.name}
+          data={products}
+          keyExtractor={product => product.id}
           renderItem={({item}) => (
             <ProductItem>
               <ProductImage
                 source={{
-                  uri:
-                    'https://static.netshoes.com.br/produtos/tenis-nike-shox-nz-eu-masculino/14/D12-9970-014/D12-9970-014_zoom1.jpg',
+                  uri: item.image,
                 }}
               />
-              <Strong>{item.name}</Strong>
-              <SpanPrice>R$129.90</SpanPrice>
+              <Strong>{item.title}</Strong>
+              <SpanPrice>{item.price}</SpanPrice>
               <CartButton>
                 <CartBasketView>
                   <Icon name="shopping-basket" size={20} color="#FFF" />
